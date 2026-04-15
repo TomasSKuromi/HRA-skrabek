078610be7e73c802bb5d29998500d6b1a0eee55d
TomasSKuromi <skrabek7@seznam.cz>
2026-04-15 18:51:24 +0200

    Add files via upload

---
+++ HRA_tomas_skrabek.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+// ---------- HRAC ----------
+
+// ---------- LEVEL SYSTEM ----------
+
+// ---------- VESNICE ----------
+
+// ---------- SCHOPNOST ----------
+
+// ---------- BOJ ----------
+
+// ---------- BOSS ----------
+
+// ---------- MAIN ----------
+int main() {
+
+
+}
